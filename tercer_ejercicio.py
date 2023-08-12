a33b5445369571feb46fc17e8ff6c1635238dbb0
Tachoviendo <ignacio.3249@gmail.com>
2023-08-12 13:09:49 -0300

    tercer_ejercicio

---
+++ tercer_ejercicio.py
@@ -0,0 +1,5 @@
+n1=int(input('ingresa un numero'))
+n2=int(input('ingresa un numero'))
+print('ingrese una opcion.\n\t 1.sumar.\n\t 2.restar\n\t 3.multiplicar\n\t 4.dividir')
+tipo_de_operacion=int(input(':'))
+
