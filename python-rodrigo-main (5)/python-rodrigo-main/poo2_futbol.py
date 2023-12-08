fe09fa10399decf9f36785b4fde59dce41527d10
rodrigocarr1on <141192202+rodrigocarr1on@users.noreply.github.com>
2023-12-08 18:01:11 -0300

    8/12

--- python-rodrigo-main (5)/python-rodrigo-main/poo2_futbol.py
+++ python-rodrigo-main (5)/python-rodrigo-main/poo2_futbol.py
@@ -1,12 +1,56 @@
+import math
 import random
-class jugadores:
-    def __init__(self, nombre, dorsal, probabilidad):
-        self.nombre=nombre
-        self.dorsal=dorsal
-        self.probabilidad=probabilidad
+
+
+
+
+
+
+
+
+
+
+
+class juego:
+    def __init__(self, n_de_j, ganador, historial,numero):
+        self.n_de_j=n_de_j
+        self.ganador=ganador
+        self.historial=historial
+        self.numero=0
+        fut=juego(2, ganador,historial)
+        fut.ganador()
+
+    def probabilidades(self,jugador1,jugador2):
+        jugador1=random.randint(1,10)
+        jugador2=random.randint(1,10)
+        self.numero=random.randint(1,10)
+
+    def ganador(self):
+        class jugadores:
+            def __init__(self, nombre, dorsal, probabilidad):
+                self.nombre=nombre
+                self.dorsal=dorsal
+                self.probabilidad=probabilidad
+            
+        jugador1=jugadores("alberto","10", random.randint(1,10))
+        jugador2= jugadores("fernando","5", random.randint(1,10))
+        print(jugador1.probabilidad)
+        print(jugador2.probabilidad)
+        probab_programa=random.randint(1,10)
+        print(probab_programa)
+        
+        res1=int(probab_programa)-int(jugador1.probabilidad)
+        print("res1=",res1)
+        res2=int(probab_programa)-int(jugador2.probabilidad)
+        print("res2=",res2)
+            
+        if math.fabs(res1)==math.fabs(res2):
+                print("es un empate")
+        elif math.fabs(res1)<math.fabs(res2):
+                print("el ganador es el jugador 1")
+        else:
+                print("el ganador es el jugador2")
     
-jugador1=jugadores("alberto","10", random.randint(1,10))
-jugador2= jugadores("fernando","5", random.randint(1,10))
-
-probab_programa=random.randint(1,10)
-
+    historial=[]
+    historial.append(ganador)
+    
