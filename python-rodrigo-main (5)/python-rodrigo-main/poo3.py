e850adba4e183a4f4cfce2224dff475079af29af
rodrigocarr1on <141192202+rodrigocarr1on@users.noreply.github.com>
2023-12-13 20:38:50 -0300

    13/12

--- python-rodrigo-main (5)/python-rodrigo-main/poo3.py
+++ python-rodrigo-main (5)/python-rodrigo-main/poo3.py
@@ -2,15 +2,19 @@ class animal:
     def __init__(self,especie,edad):
         self.especie=especie
         self.edad=edad
-    def caminar():
+    def caminar(self):
             print("caminando...")
+
 class loro(animal):
-    super() __init__ (especie,edad)
-    def volar():
-        print("volando...")
+    def __init__(self,especie, edad, ldp):
+        animal.__init__(self, especie, edad)
+        self.ldp=ldp
+
+    def volar(self):
+            print("volando...")
         
-miloro=loro("loro","6")
+miloro=loro("loro","6","2")
 miloro.volar()
 miloro.caminar()
-print(miloro.caminar)
-print(miloro.volar)
+
+
---
+++ python-rodrigo-main (5)/python-rodrigo-main/trbajo_final_poo/almacen_poo.py
@@ -0,0 +1,16 @@
+class persona:
+    def __init__(self, nombre, cedula, rol)
+        self.nombre=nombre
+        self.cedula=cedula
+        self.rol=rol
+        
+class comprador:
+    def __init__ (self,n_v)
+        self.n_v=n_v
+    
+class vendedor:
+    def __init__(self, salario)
+        self.salario=salario
+    
+class venta:
+    def __init__(self, fecha, mdp,producto, v)   #mdp=metodo de pago
